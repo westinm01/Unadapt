3d039be423d1e5e30a1d402eb98c7b819060eb40
Wes <wmont012@ucr.edu>
2022-07-22 19:06:32 -0700

    predator detection and log obstacle

--- Unadapt/Assets/Scripts/Predator/PredatorDetect.cs
+++ Unadapt/Assets/Scripts/Predator/PredatorDetect.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class PredatorDetect : MonoBehaviour
 {
     // Start is called before the first frame update
+    public ColorDetector colorDetector;
+    public bool canAttack;
     void Start()
     {
-        
+        canAttack = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        canAttack = !colorDetector.isMatching;
     }
 }
--- Unadapt/Assets/Scripts/Predator/PredatorMovement.cs
+++ Unadapt/Assets/Scripts/Predator/PredatorMovement.cs
@@ -9,36 +9,47 @@ public class PredatorMovement : MonoBehaviour
     public List<Transform> positions;
     public int positionsIndex=0;
     public float speed;
+    public float attackSpeed = 10f;
+    private float oldSpeed;
     public Transform startPos;
 
     Vector3 nextPos;
     // Start is called before the first frame update
     void Start()
     {
+        detector = GetComponent<PredatorDetect>();
         nextPos = startPos.position;
+        oldSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-         if(transform.position == positions[positionsIndex].position)
-        {
-            
-            if(positionsIndex < positions.Count - 1)
+        if(detector.canAttack){
+            nextPos = detector.colorDetector.player.transform.position;
+            speed = attackSpeed;
+        }
+        else{
+            speed = oldSpeed;
+            if(transform.position == positions[positionsIndex].position)
             {
-                //Debug.Log("index increased to " + positionsIndex);
-                positionsIndex++;
-            }
-            else{
-                //Debug.Log("Hit else");
-                if (!freezeAtEnd){
-                    positionsIndex = 0;//Goes back to start
+                
+                if(positionsIndex < positions.Count - 1)
+                {
+                    //Debug.Log("index increased to " + positionsIndex);
+                    positionsIndex++;
                 }
+                else{
+                    //Debug.Log("Hit else");
+                    if (!freezeAtEnd){
+                        positionsIndex = 0;//Goes back to start
+                    }
+                }
+                
             }
             nextPos = positions[positionsIndex].position;
+            
         }
-        
-        
         transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
     }
 }
