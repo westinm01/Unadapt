41d0922d748e9d16220400a13a068de725807447
Wes <wmont012@ucr.edu>
2022-07-22 18:41:58 -0700

    Created Matching Color Detection

--- Unadapt/Assets/Scripts/Player/ColorDetector.cs
+++ Unadapt/Assets/Scripts/Player/ColorDetector.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ColorDetector : MonoBehaviour
 {
     // Start is called before the first frame update
 
     //private Tilemap tm;
+    public Grid grid;
+    public GameObject player;
     public bool isMatching = true;
     void Start()
     {
@@ -16,7 +19,15 @@ public class ColorDetector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3Int lPos = grid.WorldToCell(player.transform.position);
+        foreach (Transform child in grid.gameObject.transform)
+        {
+            Tilemap tm = child.gameObject.GetComponent<Tilemap>();
+            if(tm.GetTile(lPos) != null)
+            {
+                isMatching = tm.color == player.GetComponent<SpriteRenderer>().color;
+            }
+        }
     }
 /*
     void OnTriggerEnter2D(Collision2D col)
